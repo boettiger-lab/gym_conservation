be5a2411651b346d1ed3ec03687605d4f02f75da
Carl <cboettig@gmail.com>
2020-12-08 06:41:33 +0000

    some examples

---
+++ examples/A2C.py
@@ -0,0 +1,20 @@
+import gym
+import numpy as np
+from stable_baselines3 import A2C
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.evaluation import evaluate_policy
+
+import gym_conservation
+
+
+env = gym.make("conservation-v2", init_state = 0.1)
+check_env(env)
+model = A2C("MlpPolicy", env, verbose=0)
+model.learn(total_timesteps=200000)
+
+## Simulate a run with the trained model, visualize result
+df = env.simulate(model)
+env.plot(df, "A2C-test.png")
+
+## Evaluate model
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=5)
