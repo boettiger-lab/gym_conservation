ed4660c7c3178bee39703c5144cac4336b11a1ed
Carl <cboettig@gmail.com>
2020-12-16 00:05:08 +0000

    more challenging default parameters

--- examples/tipping.py
+++ examples/tipping.py
@@ -1,21 +1,21 @@
 import gym
 import numpy as np
-from stable_baselines3 import A2C, PPO
+from stable_baselines3 import A2C, PPO, DDPG
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.evaluation import evaluate_policy
 
 import gym_conservation
 
 
-env = gym.make("conservation-v5", sigma=0.01)
+env = gym.make("conservation-v5", beta = 0.45, sigma=0.01, a = .22)
 check_env(env)
-model = A2C("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=80000)
+model = DDPG("MlpPolicy", env, verbose=1)# , policy_kwargs = dict(net_arch=[256, 256]))
+model.learn(total_timesteps=100000)
 
 ## Simulate a run with the trained model, visualize result
 df = env.simulate(model, reps = 10)
-env.plot(df, "PPO-v5-test.png")
+env.plot(df, "DDPG-v5-test.png")
 
 ## Evaluate model
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=50)
-print("algo:", "PPO", "env:", "conservation-v2", "mean reward:", mean_reward, "std:", std_reward)
+print("algo:", "DDPG", "env:", "conservation-v2", "mean reward:", mean_reward, "std:", std_reward)
