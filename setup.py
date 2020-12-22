084ce3c3ab8f33b39009e52fd9db077ee8d92162
Carl <cboettig@gmail.com>
2020-12-22 21:05:41 +0000

    don't duplicate the key

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import setuptools
 
 setuptools.setup(
     name="gym_conservation",
-    packages=["gym_conservation"],
     version="0.0.2.1",
     license="MIT",
     description="Provide gym environments for reinforcement learning",
