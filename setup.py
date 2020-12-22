af45c660e0afbcb028df49db6f980f58f437f3d4
Carl <cboettig@gmail.com>
2020-12-22 19:07:00 +0000

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="gym_conservation",
     packages=["gym_conservation"],
-    version="0.0.2",
+    version="0.0.2.1",
     license="MIT",
     description="Provide gym environments for reinforcement learning",
     author="Carl Boettiger & Marcus Lapeyrolerie",
