f88837d18cac4ab1818f8f4926388ed55b99c528
Carl Boettiger <cboettig@gmail.com>
2021-04-07 22:33:56 +0000

    format

--- examples/A2C.py
+++ examples/A2C.py
@@ -7,15 +7,15 @@ from stable_baselines3.common.evaluation import evaluate_policy
 import gym_conservation
 
 
-env = gym.make("conservation-v5")
+env = gym.make("conservation-v2")
 check_env(env)
 model = A2C("MlpPolicy", env, verbose=0)
-model.learn(total_timesteps=300000)
+model.learn(total_timesteps=3000000)
 
 ## Simulate a run with the trained model, visualize result
 df = env.simulate(model)
-env.plot(df, "results/v5-A2C.png")
+env.plot(df, "results/v2-A2C.png")
 
 ## Evaluate model
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=50)
-print("A2C, v5", "mean reward:", mean_reward, "std:", std_reward)
+print("A2C, v2", "mean reward:", mean_reward, "std:", std_reward)
