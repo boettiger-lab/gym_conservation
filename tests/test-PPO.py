150b747387b46179e63d087f6a539704aa1c2342
Carl Boettiger <cboettig@gmail.com>
2021-04-09 20:47:53 +0000

    fix check in updated env

--- tests/test-PPO.py
+++ tests/test-PPO.py
@@ -2,6 +2,7 @@ import gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
 
 
 def test_ppo():
@@ -11,5 +12,7 @@ def test_ppo():
     model.learn(total_timesteps=200)
     df = env.simulate(model)
     env.plot(df, "PPO-test.png")
-    mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=5)
+    mean_reward, std_reward = evaluate_policy(
+        model, Monitor(env), n_eval_episodes=5
+    )
     env.plot_policy(df, "PPO-policy.png")
--- tests/test-models.py
+++ tests/test-models.py
@@ -1,6 +1,7 @@
 import gym
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
 
 import gym_conservation
 from gym_conservation.models.policies import fixed_action, target_state
@@ -12,7 +13,7 @@ def test_fixed_action():
     model = fixed_action(env)
     df = env.simulate(model, reps=2)
     env.plot(df, "fixed_action-test.png")
-    evaluate_policy(model, env, n_eval_episodes=50)
+    evaluate_policy(model, Monitor(env), n_eval_episodes=50)
 
 
 def test_target_state():
@@ -21,4 +22,4 @@ def test_target_state():
     model = target_state(env, 0.8)
     df = env.simulate(model, reps=2)
     env.plot(df, "fixed_state-test.png")
-    evaluate_policy(model, env, n_eval_episodes=50)
+    evaluate_policy(model, Monitor(env), n_eval_episodes=50)
